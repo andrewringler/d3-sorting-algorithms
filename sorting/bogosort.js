a0278dba022fb86abb374e11a5650ef1ab61741e
Andrew Ringler <ringler@gmail.com>
2012-09-30 17:00:20 -0400

    bogosort, WIP, need to make this one use a buffer, so we can animate while it is running since it will probably never complete

---
+++ sorting/bogosort.js
@@ -0,0 +1,29 @@
+// http://en.wikipedia.org/wiki/Bogosort
+function bogosort(array) {
+  var actions = [];
+
+  function inOrder() {
+	for(var i=1; i<array.length; i++) {
+		actions.push({type: "traverse", "traverse": i});
+		if(array[i] < array[i-1]) {
+			return false;
+		}
+	}
+	return true;
+  }
+
+  function bogo() {
+	var tries = 0;
+	while(!inOrder()){
+		if(tries > 10) {
+			return;
+		}
+		tries++;
+		// TODO capture operations in shuffle
+		shuffle(array, actions);
+	}
+  }
+
+  bogo();
+  return actions;
+}
--- sorting/fisher-yates.shuffle.js
+++ sorting/fisher-yates.shuffle.js
@@ -1,12 +1,13 @@
 // Fisher–Yates shuffle
 // see http://bl.ocks.org/1582075
-function shuffle(array) {
+function shuffle(array, actions) {
   var i = array.length, j, t;
   while (--i > 0) {
     j = ~~(Math.random() * (i + 1));
     t = array[j];
     array[j] = array[i];
     array[i] = t;
+	if(actions) actions.push({type: "swap", i: i, j: j});
   }
   return array;
 }
