f9d7a9b3f03a0d0a036e553185a2bc87757b4443
Andrew Ringler <ringler@gmail.com>
2012-10-01 00:16:44 -0400

    styling

--- src/bogosort.js
+++ src/bogosort.js
@@ -2,6 +2,21 @@
 function bogosort(array) {
   var actions = [];
 
+  	// Fisher–Yates shuffle
+	// taken from http://bl.ocks.org/1582075
+	function shuffle(array, actions) {
+	  var i = array.length, j, t;
+	  while (--i > 0) {
+	    j = ~~(Math.random() * (i + 1));
+	    t = array[j];
+	    array[j] = array[i];
+	    array[i] = t;
+		if(actions) actions.push({type: "shuffle", i: i, j: j});
+	  }
+	  return array;
+	}
+
+
   function inOrder() {
 	for(var i=1; i<array.length; i++) {
 		actions.push({type: "traverse", "traverse": i});
